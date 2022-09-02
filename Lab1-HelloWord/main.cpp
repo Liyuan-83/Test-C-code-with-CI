8883b1c911356110c025f286f83b55032017e4f8
liyuan_chang <liyuan_chang@transcend-info.com>
2022-09-02 14:59:49 +0800

    test CI

--- Lab1-HelloWord/main.cpp
+++ Lab1-HelloWord/main.cpp
@@ -1,10 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
 #include "stdlib.h"
 
 int main() {
 	int i;
 	printf("Hello World\n");
-	scanf_s("%d", &i);
+	scanf("%d", &i);
 	printf("input is %d\n", i);
 	system("pause");
 	return 0;
